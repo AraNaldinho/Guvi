3bd3daba00e7eaabe0f112eb7527c610b842b3ec
Aravind <aravind2792@hotmail.com>
2016-09-10 19:22:32 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def isPalindrome(int x) {
+        s = str(x)
+        i = 0
+        j = len(s)-1
+        r = True
+        while i < j:
+            if s[i] != s[j]:
+                r = False
+                break
+            i += 1
+            j -= 1
+        return r
+}
